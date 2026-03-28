380e5b63b2debef47634da9365489cb25fe14225
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-27 18:13:57 -0700

    Created.

---
+++ services/Ollama/ollamaUpdate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Script to update the Ollama container
+# N.B. Must run this on the DGX Spark machine
+
+
+VERSION=${1:-latest}
+
+COMPOSE_FILE="${HOME}/Code/DGX_Spark/services/Ollama_OpenWebUI/docker-compose.yml"
+
+docker compose -f "${COMPOSE_FILE}" down
+
+docker rm ollama
+
+docker pull ollama/ollama:${VERSION}
+
+docker compose -f "${COMPOSE_FILE}" up -d
+
