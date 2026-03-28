b1f278ed18c54f910343ec0d69590301610476f0
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-27 17:47:44 -0700

    Made curl silent.

--- services/Ollama/ollamaModels.sh
+++ services/Ollama/ollamaModels.sh
@@ -3,4 +3,4 @@
 # Script to list the models currently loaded in the Ollama container
 
 
-curl http://${HOSTNAME:-spark-8d0d.lan}:11434/api/tags | jq -r '.models[].name'
+curl -s http://${HOSTNAME:-spark-8d0d.lan}:11434/api/tags | jq -r '.models[].name'
