a884a9a8a3cccb1128058ccedf0e79b43756f3ca
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-27 17:57:20 -0700

    Cleaned up.

--- services/Ollama/ollamaModels.sh
+++ services/Ollama/ollamaModels.sh
@@ -2,5 +2,12 @@
 #
 # Script to list the models currently loaded in the Ollama container
 
+HOSTNAME=${1:-spark-8d0d.lan}
+PORTNUM=${2:-11434}
 
-curl -s http://${HOSTNAME:-spark-8d0d.lan}:11434/api/tags | jq -r '.models[].name'
+if ! nc -z -w2 "$HOSTNAME" "$PORTNUM" &>/dev/null; then
+    echo "ERROR: host '$HOSTNAME:$PORTNUM' is not reachable or invalid"
+    exit 1
+fi
+
+curl -s http://${HOSTNAME}:${PORTNUM}/api/tags | jq -r '.models[].name'
