abb068c3ee63a357f44eed9499880c3c9693fef2
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-27 18:02:22 -0700

    Created.

---
+++ services/Ollama/ollamaVersion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Script to print the version of the currently running Ollama instance
+
+HOSTNAME=${1:-spark-8d0d.lan}
+PORTNUM=${2:-11434}
+
+if ! nc -z -w2 "$HOSTNAME" "$PORTNUM" &>/dev/null; then
+    echo "ERROR: host '$HOSTNAME:$PORTNUM' is not reachable or invalid"
+    exit 1
+fi
+
+curl -s http://${HOSTNAME}:${PORTNUM}/api/version
+echo ""
