d8174851e22d91883f0c63fcd2a1d023bac7444e
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-30 14:45:08 -0700

    Created.

---
+++ services/OpenWebUI/openWebUiUpdate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Script to update the Open-WebUI container
+# N.B. Must run this on the DGX Spark machine
+
+
+VERSION=${1:-latest}
+
+COMPOSE_FILE="${HOME}/Code/DGX_Spark/services/Ollama_OpenWebUI/docker-compose.yml"
+
+docker compose -f "${COMPOSE_FILE}" stop open-webui
+
+docker rm open-webui
+
+docker pull ghcr.io/open-webui/open-webui:${VERSION}
+
+docker compose -f "${COMPOSE_FILE}" up -d open-webui
